a2912b796b5b355e82dccc9435f30cc78f0102b1
freegame01995 <32276095-freegame01995@users.noreply.replit.com>
2024-02-29 00:48:11 +0000

    criei o programa da media do iesb

---
+++ aulas/aulas01/meu_programa.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+float nota1 = 0.0;
+float nota2 = 0.0;  
+
+printf("entre com a nota1: ");
+scanf("%f", &nota1);
+
+printf("entre com a nota2: ");
+scanf("%f", &nota2);
+
+float media = 0.4*nota1 + 0.6*nota2;
+
+printf("A média final é %.1f", media);
+
+  if (media >= 5.0) {
+  printf("passei!\n");
+ } else {
+    printf("não passei!\n");
+ }
+  return 0;
+ }
